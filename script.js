76990b5fefef6e962462acd7bdfa1c6507ac73c2
deondaci <deondaci1@gmail.com>
2024-03-05 06:16:57 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let mosha = 23
+// camal case
+let numridhomes
+let numri_dhomes;
+
+// operatori, -,+ etj
+// operant,,, numrat
+//  operatori binar--2 operant
+
+// 5 == '5'  -true
+// 5 === '5'  -false  (kqyr edhe llojin e infos)
+ 
+//  deklarimi i me shum se 1 variable behet me presje(,)
+
+let moshaRedonit, moshaYlli, moshaJona, moshaGerta, moshaArlindit;
+moshaRedonit = 2023 - 2006;
+moshaYlli = 2023 - 2008;
+moshaJona = 2023 - 2007;
+moshaGerta = 2023 - 2004;
+moshaArlindit = 2023 - 2007;
+
+console.log(moshaRedonit, moshaYlli, moshaJona, moshaGerta, moshaArlindit)
+//  (;) tregon qe qaty perfundon rreshti
+
+let a = 25
+let b = 4
+let c = '7'
+let bool = false
+
+let mbledhja = a + b;
+// rez = 29
+console.log('rezlutati eshte : ' +mbledhja)
+// rez = rezlutati eshte : 29
+
+//  string + number
+console.log(a + c)
+//  rez = 257
+
+// bene konvertimin e te gjitha variablave ne number
+console.log(typeof +c)
+// rez = number
+
+console.log(bool)
+//  rez = 0,
+// false = 0,,,,,,,,true = 1
+
+console.log('mbledhja ' +(4+3))
+console.log('zbritja ' +(4-3))
+console.log('shumezimi ' +(4*3))
+console.log('pjestimi ' +(4/3))
+console.log('fuqia ' +(4**3))
+console.log('modular ' +(4%3))
